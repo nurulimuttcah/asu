3563aa197f00544b2c7405b5463d4528b4ea8e3b
nurulimuttcah <132120130+nurulimuttcah@users.noreply.github.com>
2023-05-19 22:32:38 +0800

    Add files via upload

---
+++ menu/about.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+echo -e "\e[1;31m   🔰 Untuk 10 64 bit                               \e[m"
+echo -e "\e[1;31m   🔰 Untuk Ubuntu 18.04 & Ubuntu 20.04 64bit       \e[m"
+echo -e "\e[1;31m   🔰 Untuk VPS dengan virtualisasi KVM dan VMWare  \e[m"
+echo -e "\e[1;31m   🔰 XlordProject X Nielaa                        \e[m"
+echo -e "\e[1;31m   🔰 t.me/xlordeuyy                                  \e[m"
+echo ""
+read -n 1 -s -r -p "Enter Untuk Kembali Ke Menu Utama"
+menu
